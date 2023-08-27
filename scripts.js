de7139685c83420b5770319cd13f75c77544d370
Karen <61708275+karenmq@users.noreply.github.com>
2023-08-27 14:32:22 -0700

    hopefully final version of push
    The last of the design tweaks!

--- scripts.js
+++ scripts.js
@@ -13,34 +13,3 @@ $(document).on("scroll", function() {
     }
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// COLLAPSIBLE
-// var collapse = document.getElementsByClassName("collapsible");
-
-// var n;
-
-// for (n = 0; n < collapse.length; n++) {
-//   collapse[n].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.maxHeight) {
-//         content.style.maxHeight = null;
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     }
-//   });
-// }
-
